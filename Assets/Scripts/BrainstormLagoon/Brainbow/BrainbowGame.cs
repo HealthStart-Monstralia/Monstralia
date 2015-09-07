1d0f30eb5cf0c0f3e191082c2b1f3463da5a2cda
Angie Murphy <angiemurphy@utexas.edu>
2015-09-07 12:35:18 -0500

    implementing new brainbow script
    pointless right now, but will contain useful things later

---
+++ Assets/Scripts/BrainstormLagoon/Brainbow/BrainbowGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrainbowGame : MonoBehaviour {
+
+	public GameObject instructionsCanvas;
+
+	// Show demonstration.
+	void Awake(){
+		// print ("in Awake of Brainbow game");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// print ("in Update of Brainbow game");
+		if (Time.timeSinceLevelLoad < 3.0f)
+			print ("instructionsCanvas.name: " + instructionsCanvas.name);
+	}
+}
