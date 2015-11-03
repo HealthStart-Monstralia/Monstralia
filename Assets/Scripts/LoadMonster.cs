63c85db387d65b77a183ed24db5be99cea5d8b7a
Jacob Bobb <jaybobb710@utexas.edu>
2015-11-03 02:53:10 -0600

    added stickers page as a new scene

--- Assets/Scripts/LoadMonster.cs
+++ Assets/Scripts/LoadMonster.cs
@@ -5,6 +5,7 @@ public class LoadMonster : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
+		Debug.Log ("in Awake of load monster");
 		Sprite monsterSprite = Resources.Load<Sprite>(GameManager.instance.getMonster());
 		gameObject.GetComponent<SpriteRenderer>().sprite = monsterSprite;
 	}
---
+++ Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveableObject : MonoBehaviour {
+
+	private Vector3 offset;
+	private Vector3 screenPoint;
+	private bool moving;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown() {
+		moving = true;
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	void FixedUpdate() {
+		if(moving) {
+			Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+			gameObject.GetComponent<Rigidbody2D>().MovePosition(curPosition);
+		}
+	}
+
+	void OnMouseUp() {
+		moving = false;
+	}
+}
