44054de916f739a7a484f1c90b81503c44df08ce
Jacob Bobb <jaybobb710@utexas.edu>
2016-01-09 16:32:47 -0600

    moved countdown code to separate script in MemoryMatch

--- Assets/Scripts/BrainstormLagoon/MemoryMatch/MemoryMatchGameManager.cs
+++ Assets/Scripts/BrainstormLagoon/MemoryMatch/MemoryMatchGameManager.cs
@@ -99,28 +99,28 @@ public class MemoryMatchGameManager : MonoBehaviour {
 		foreach(GameObject d in dishes) {
 			d.GetComponent<DishBehavior>().top.GetComponent<SpriteRenderer>().enabled = true;
 		}
-		//CountdownManager.GetInstance().BeginCountdown();
-		GameObject countdown3 = (GameObject)Instantiate(Resources.Load("Countdown3")); 
-		countdown3.SetActive (true);
-		yield return new WaitForSeconds (1.0f);
-		countdown3.SetActive (false);
-		
-		GameObject countdown2 = (GameObject)Instantiate(Resources.Load("Countdown2"));
-		countdown2.SetActive (true);
-		yield return new WaitForSeconds (1.0f);
-		countdown2.SetActive (false);
-		
-		GameObject countdown1 = (GameObject)Instantiate(Resources.Load("Countdown1"));
-		countdown1.SetActive (true);
-		yield return new WaitForSeconds (1.0f);
-		countdown1.SetActive (false);
-		
-		GameObject countdownGo = (GameObject)Instantiate(Resources.Load("CountdownGo"));
-		countdownGo.SetActive (true);
-		yield return new WaitForSeconds (1.0f);
-		countdownGo.SetActive (false); 
+//		GameObject countdown3 = (GameObject)Instantiate(Resources.Load("Countdown3")); 
+//		countdown3.SetActive (true);
+//		yield return new WaitForSeconds (1.0f);
+//		countdown3.SetActive (false);
+//		
+//		GameObject countdown2 = (GameObject)Instantiate(Resources.Load("Countdown2"));
+//		countdown2.SetActive (true);
+//		yield return new WaitForSeconds (1.0f);
+//		countdown2.SetActive (false);
+//		
+//		GameObject countdown1 = (GameObject)Instantiate(Resources.Load("Countdown1"));
+//		countdown1.SetActive (true);
+//		yield return new WaitForSeconds (1.0f);
+//		countdown1.SetActive (false);
+//		
+//		GameObject countdownGo = (GameObject)Instantiate(Resources.Load("CountdownGo"));
+//		countdownGo.SetActive (true);
+//		yield return new WaitForSeconds (1.0f);
+//		countdownGo.SetActive (false); 
 
 		gameStartup = false;
+		StartCoroutine(gameObject.GetComponent<Countdown>().RunCountdown());
 		gameStarted = true;
 
 		timer.StartTimer();
