bd6598b6d7dbd29ac913548ddd50c8118adfc758
Jacob Bobb <jaybobb710@utexas.edu>
2016-01-11 22:19:32 -0600

    added temporary voice-over audio for foods

--- Assets/Scripts/LoadMonster.cs
+++ Assets/Scripts/LoadMonster.cs
@@ -5,7 +5,6 @@ public class LoadMonster : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-		Debug.Log ("in Awake of load monster");
 		Sprite monsterSprite = Resources.Load<Sprite>(GameManager.instance.getMonster());
 		gameObject.GetComponent<SpriteRenderer>().sprite = monsterSprite;
 	}
