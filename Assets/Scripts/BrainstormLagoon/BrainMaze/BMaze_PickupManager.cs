aba55faea42476706848a72712c056f94f11e01a
Colby Tang <colby.us2012@gmail.com>
2016-11-17 14:16:12 -0600

    Tied score gauge to amount of pickups in the maze.

--- Assets/Scripts/BrainstormLagoon/BrainMaze/BMaze_PickupManager.cs
+++ Assets/Scripts/BrainstormLagoon/BrainMaze/BMaze_PickupManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,20 +9,32 @@ public class BMaze_PickupManager : MonoBehaviour {
 	 */
 
 	public GameObject door;
+	public Slider scoreGauge;
 	public List<GameObject> pickupList = new List<GameObject> ();
 
 	private AudioSource audioSrc;
+	private int initialPickupCount;
+	private int pickupCount;
 
 	void Start () {
 		audioSrc = GetComponent<AudioSource> ();
 		for (int count = 0; count < transform.childCount; count++) {
 			pickupList.Add (transform.GetChild (count).gameObject);
 		}
+		initialPickupCount = pickupList.Count;
+		scoreGauge.value = 0f;
 	}
 
 	void Update () {
 		if (pickupList.Count <= 0 && door.gameObject) {
 			OpenDoor ();
+		}
+
+		if (pickupList.Count != 0 && pickupList.Count != pickupCount) {
+			pickupCount = pickupList.Count;
+			scoreGauge.value = 1 - ((float)pickupCount / (float)initialPickupCount);
+		} else if (pickupList.Count == 0 && pickupList.Count != pickupCount) {
+			scoreGauge.value = 1;
 		}
 	}
 
