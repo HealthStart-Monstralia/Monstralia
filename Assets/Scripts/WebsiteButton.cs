7f5e9d097fded95548b392fd4b65f0134e58ffba
mariaaddison <mgpaddison@gmail.com>
2017-06-21 14:21:24 -0500

    Website Button
    The website button on the Parents Page now take you to the Monstrila page.

---
+++ Assets/Scripts/WebsiteButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebsiteButton : MonoBehaviour {
+
+	public void websiteButton()
+	{
+		Application.OpenURL ("http://www.healthstartfoundation.org/monstraliatrade.html");
+	}
+}
